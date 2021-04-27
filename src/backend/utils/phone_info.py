58247cc15ef985518bb61b54674ecc51028036ae
Sujal Singh <email.sujalsingh@gmail.com>
2021-04-27 16:56:53 +0530

    phone info collection script added

---
+++ src/backend/utils/phone_info.py
@@ -0,0 +1,21 @@
+from run import run
+
+
+def get_phone_info():
+    wifi_status = True if "enabled" in run("adb shell dumpsys wifi | sed -n '1p'")[0] else False
+
+    charging_status = {
+        "AC": True if "true" in run('adb shell dumpsys battery | grep "AC powered"')[0] else False,
+        "USB": True if "true" in run('adb shell dumpsys battery | grep "USB powered"')[0] else False
+    }
+
+    charging_status = charging_status["AC"] or charging_status["USB"]
+    battery_level = int(run('adb shell dumpsys battery | grep "level"')[0].replace("  level: ", ""))
+
+    phone_info = {
+        "wifi_status": wifi_status,
+        "charging_status": charging_status,
+        "battery_level": battery_level,
+    }
+
+    return phone_info
